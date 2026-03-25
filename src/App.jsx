b8421ff8e99198f63dd3fe1e8214551b2facf9a1
USER-28 <USER-28@growmore.local>
2026-03-25 12:35:51 +0530

    project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
+import ScrollToTop from "./Components/ScrollToTop";
 
+// Pages
 import Home from "./Pages/Home";
 import WhoWeAre from "./Pages/WhoWeAre";
 import Migrate from "./Pages/Migrate";
@@ -21,13 +23,25 @@ import Blogs from "./Pages/Blogs";
 import ContactUs from "./Pages/ContactUs";
 import BookConsultation from "./Pages/BookConsultation";
 import Brochures from "./Pages/Brochures";
+import Terms from "./Pages/Terms";
+import Policy from "./Pages/Policy";
+import Refund from "./Pages/Refund";
 
+// Articles
+import NewsArticle from "./Pages/NewsArtical";
+import BlogsArtical from "./Pages/BlogsArtical";
+
+// Components
 import Checklist from "./Components/Checklist";
 import Checklistgsm from "./Components/Checklistgsm";
 import Checklistlabour from "./Components/Checklistlabour";
+
+// Student Pages
 import StudentAustralia from "./Pages/StudentAustralia";
 import StudentDubai from "./Pages/StudentDubai";
 import StudentUk from "./Pages/StudentUk";
+
+// Employer Pages
 import EmployerVisaBusiness from "./Pages/EmployerVisaBusiness";
 import EmployerVisaLabour from "./Pages/EmployerVisaLabour";
 import EmployerVisaEmployer from "./Pages/EmployerVisaEmployer";
@@ -35,6 +49,8 @@ import EmployerVisaRegional from "./Pages/EmployerVisaRegional";
 import EmployerVisaStrategic from "./Pages/EmployerVisaStrategic";
 import EmployerVisaTraining from "./Pages/EmployerVisaTraining";
 import EmployerVisaLMT from "./Pages/EmployerVisaLMT";
+
+// Individual Pages
 import IndividualVisaEmployer from "./Pages/IndividualVisaEmployer";
 import IndividualVisaGsm from "./Pages/IndividualVisaGsm";
 import IndividualVisaFamily from "./Pages/IndividualVisaFamily";
@@ -45,22 +61,19 @@ import IndividualVisaBusiness from "./Pages/IndividualVisaBusiness";
 import IndividualVisaRefugee from "./Pages/IndividualVisaRefugee";
 import IndividualVisaETA from "./Pages/IndividualVisaETA";
 import IndividualVisaGurdian from "./Pages/IndividualVisaGurdian";
-import Terms from "./Pages/Terms";
-import Policy from "./Pages/Policy";
-import Refund from "./Pages/Refund";
-import NewsArticle from "./Pages/NewsArtical";
-import BlogsArtical from "./Pages/BlogsArtical";
-import ScrollToTop from "./Components/ScrollToTop";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <ScrollToTop />
+
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="who-we-are" element={<WhoWeAre />} />
+        <Route path="/who-we-are" element={<WhoWeAre />} />
         <Route path="/migrate-to-australia" element={<Migrate />} />
+
+        {/* Services */}
         <Route path="/our-services/individual" element={<IndividualVisa />} />
         <Route path="/our-services/employers" element={<EmployerVisa />} />
         <Route path="/our-services/student-visa" element={<StudentVisa />} />
@@ -73,13 +86,23 @@ function App() {
           path="/our-services/labour-agreement"
           element={<LabourAgreement />}
         />
+
+        {/* Tools */}
         <Route path="/points-calculator" element={<PointsCalculator />} />
-        <Route path="videos" element={<Videos />} />
-        <Route path="news" element={<News />} />
-        <Route path="blogs" element={<Blogs />} />
+
+        {/* Media */}
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/news/:slug" element={<NewsArticle />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/blogs/:slug" element={<BlogsArtical />} />
+
+        {/* Contact */}
         <Route path="/contact-us" element={<ContactUs />} />
         <Route path="/book-consultation" element={<BookConsultation />} />
         <Route path="/brochures" element={<Brochures />} />
+
+        {/* Checklists */}
         <Route path="/checklist-for-dama" element={<Checklist />} />
         <Route
           path="/checklist-for-gsm-general-skilled-migration"
@@ -89,95 +112,102 @@ function App() {
           path="/checklist-for-labour-agreement"
           element={<Checklistlabour />}
         />
+
+        {/* Policies */}
         <Route path="/terms-services" element={<Terms />} />
         <Route path="/privacy-policy" element={<Policy />} />
-        <Route path="/cancellation-and-refund-policy" element={<Refund />} />
         <Route
-          path="/our-services/student-visa/student-visa-australia/"
+          path="/cancellation-and-refund-policy"
+          element={<Refund />}
+        />
+
+        {/* Student Pages */}
+        <Route
+          path="/our-services/student-visa/student-visa-australia"
           element={<StudentAustralia />}
         />
         <Route
-          path="/our-services/student-visa/student-visa-dubai/"
+          path="/our-services/student-visa/student-visa-dubai"
           element={<StudentDubai />}
         />
         <Route
-          path="/our-services/student-visa/student-visa-uk/"
+          path="/our-services/student-visa/student-visa-uk"
           element={<StudentUk />}
-        />{" "}
-        /
+        />
+
+        {/* Employer Pages */}
         <Route
-          path="/our-services/employers/standard-business-sponsorship/"
+          path="/our-services/employers/standard-business-sponsorship"
           element={<EmployerVisaBusiness />}
         />
         <Route
-          path="/our-services/employers/labour-agreement/"
+          path="/our-services/employers/labour-agreement"
           element={<EmployerVisaLabour />}
-        />{" "}
+        />
         <Route
-          path="/our-services/employers/nomination-application/"
+          path="/our-services/employers/nomination-application"
           element={<EmployerVisaEmployer />}
-        />{" "}
+        />
         <Route
-          path="/our-services/employers/regional-certifying-body-approval/"
+          path="/our-services/employers/regional-certifying-body-approval"
           element={<EmployerVisaRegional />}
-        />{" "}
+        />
         <Route
-          path="/our-services/employers/business-case-preparation/"
+          path="/our-services/employers/business-case-preparation"
           element={<EmployerVisaStrategic />}
         />
         <Route
-          path="/our-services/employers/training-plan/"
+          path="/our-services/employers/training-plan"
           element={<EmployerVisaTraining />}
         />
         <Route
-          path="/our-services/employers/labour-market-testing-lmt-for-sc494-and-sc482-visas/"
+          path="/our-services/employers/labour-market-testing-lmt-for-sc494-and-sc482-visas"
           element={<EmployerVisaLMT />}
         />
+
+        {/* Individual Pages */}
         <Route
-          path="/our-services/individual/employer-sponsored-visa/"
+          path="/our-services/individual/employer-sponsored-visa"
           element={<IndividualVisaEmployer />}
         />
         <Route
-          path="/our-services/individual/general-skilled-visa/"
+          path="/our-services/individual/general-skilled-visa"
           element={<IndividualVisaGsm />}
         />
         <Route
-          path="/our-services/individual/family-visa/"
+          path="/our-services/individual/family-visa"
           element={<IndividualVisaFamily />}
         />
         <Route
-          path="/our-services/individual/temporary-work-short-stay-visa/"
+          path="/our-services/individual/temporary-work-short-stay-visa"
           element={<IndividualVisaWork />}
         />
         <Route
-          path="/our-services/individual/global-talent-visa/"
+          path="/our-services/individual/global-talent-visa"
           element={<IndividualVisaGlobal />}
         />
         <Route
-          path="/our-services/individual/working-holiday-work-holiday-visa/"
+          path="/our-services/individual/working-holiday-work-holiday-visa"
           element={<IndividualVisaHoliday />}
         />
         <Route
-          path="/our-services/individual/business-investor-visa/"
+          path="/our-services/individual/business-investor-visa"
           element={<IndividualVisaBusiness />}
         />
         <Route
-          path="/our-services/individual/refugee-visa/"
+          path="/our-services/individual/refugee-visa"
           element={<IndividualVisaRefugee />}
         />
         <Route
-          path="/our-services/individual/visitor-evisitor-eta-visa/"
+          path="/our-services/individual/visitor-evisitor-eta-visa"
           element={<IndividualVisaETA />}
         />
         <Route
-          path="/our-services/individual/student-guardian-visa/"
+          path="/our-services/individual/student-guardian-visa"
           element={<IndividualVisaGurdian />}
         />
-        <Route path="/blogs" element={<Blogs />} />
-        <Route path="/blogs/:slug" element={<BlogsArtical />} />
-        <Route path="/news" element={<News />} />
-        <Route path="/news/:slug" element={<NewsArticle />} />
       </Routes>
+
       <Footer />
     </Router>
   );
