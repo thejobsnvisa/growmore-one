9ecc2888932d82bd62c38179637ef703eaf9266a
USER-28 <USER-28@growmore.local>
2026-04-27 09:03:51 +0530

    code added

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React, { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet-async'   // 👈 add this
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <HelmetProvider>   {/* 👈 wrap App */}
-      <App />
-    </HelmetProvider>
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
+);
