883bc91acf8f378afc71845681a3271be980cfbe
USER-28 <USER-28@growmore.local>
2026-04-27 11:45:05 +0530

    updated code

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
-import React, { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.jsx";
 
-/* Faster Root Render */
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </StrictMode>
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>
 );
