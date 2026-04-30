09040aca8f61cd2b8c181a6e31071c6099b7352e
USER-28 <USER-28@growmore.local>
2026-04-30 10:21:21 +0530

    code added

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,13 @@
-import React, { StrictMode, useEffect } from "react";
-import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter } from "react-router-dom";
+import React, { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { HelmetProvider } from 'react-helmet-async'   // 👈 add this
+import './index.css'
+import App from './App.jsx'
 
-import "./index.css";
-import App from "./App.jsx";
-
-/* ------------------------------
-   Restore GitHub Pages 404 route
------------------------------- */
-function RedirectHandler({ children }) {
-  useEffect(() => {
-    const redirect = sessionStorage.getItem("redirect");
-
-    if (redirect) {
-      sessionStorage.removeItem("redirect");
-      window.history.replaceState(null, "", redirect);
-    }
-  }, []);
-
-  return children;
-}
-
-/* ------------------------------ */
-
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HelmetProvider>
-      <BrowserRouter>
-        <RedirectHandler>
-          <App />
-        </RedirectHandler>
-      </BrowserRouter>
+    <HelmetProvider>   {/* 👈 wrap App */}
+      <App />
     </HelmetProvider>
-  </StrictMode>
-);
+  </StrictMode>,
+)
